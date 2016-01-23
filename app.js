b01de8b9732b53a6f2cf53e6f3ff93111d520004
Renan Monteiro Pinto Neto <renan@MBP-TX3.local>
2016-01-23 15:34:40 -0200

    Converting speech to text working

--- app.js
+++ app.js
@@ -1,29 +1,81 @@
-/*eslint-env node*/
+/**
+ * Copyright 2015 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-//------------------------------------------------------------------------------
-// node.js starter application for Bluemix
-//------------------------------------------------------------------------------
+'use strict';
 
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
-var express = require('express');
+var express      = require('express'),
+    app          = express(),
+    cors         = require('cors'),
+    vcapServices = require('vcap_services'),
+    extend       = require('util')._extend,
+    watson       = require('watson-developer-cloud');
 
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
+// Bootstrap application settings
+require('./config/express')(app);
 
-// create a new express server
-var app = express();
 
-// serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
+app.use(cors());
 
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
+// Add headers
+app.use(function (req, res, next) {
 
-// start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
-	// print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
 });
+
+// For local development, replace username and password
+var config = extend({
+  version: 'v1',
+  url: "https://stream.watsonplatform.net/speech-to-text/api",
+  username: '<username>',
+  password: '<password>'
+}, vcapServices.getCredentials('speech_to_text'));
+
+var authService = watson.authorization(config);
+
+app.get('/', function(req, res) {
+  res.render('index', { ct: req._csrfToken });
+});
+
+// Get token using your credentials
+app.post('/api/token', function(req, res, next) {
+  authService.getToken({url: config.url}, function(err, token) {
+    if (err)
+      next(err);
+    else
+      res.send(token);
+  });
+});
+
+// error-handler settings
+require('./config/error-handler')(app);
+
+var port = process.env.VCAP_APP_PORT || 3000;
+app.listen(port);
+console.log('listening at:', port);
