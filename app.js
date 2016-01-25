f5325fa803de206790a012a5df23067f82627d52
Renan Monteiro Pinto Neto <renan@MBP-TX3.local>
2016-01-25 19:44:41 -0200

    - Ui fixing - User and password of relation extract has changed Response from GET/POST changed

--- app.js
+++ app.js
@@ -37,11 +37,7 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 
 var watson = require('watson-developer-cloud');
 
-var relationship_extraction = watson.relationship_extraction({
-  username: '78d04483-93a2-4d25-8cc7-f284dba0206b',
-  password: 'rjAjJtihbzRg',
-  version: 'v1-beta'
-});
+
 
 var mongo = process.env.VCAP_SERVICES;
 var port = process.env.PORT || 3030;
@@ -65,92 +61,125 @@ if (mongo) {
 var MongoClient = require('mongodb').MongoClient;
 var db; 
 
-MongoClient.connect(conn_str, function(err, database) {
-  if(err) throw err;
-  db = database;
-  
-}); 
+ 
+var reUser = "";
+var rePass = "";
+var re = process.env.VCAP_SERVICES;
+if (re) {
+  var env = JSON.parse(re);
+  if (env['relationship_extraction']) {
+    re = env['relationship_extraction'][0]['credentials'];
+    reUser = re.username;
+    rePass = re.password;
+  } else {
+    console.error("No relationship_extraction service founded. Please check your project");
+  }
+} else {
+  console.error("No VCAP_SERVICES founded. Please check your project");;
+}
 
-
+var relationship_extraction = watson.relationship_extraction({
+  username: reUser,
+  password: rePass,
+  version: 'v1-beta'
+});
 
 app.get('/api/getMessages', function (req, res) {
-  if (db && db !== "null" && db !== "undefined") {
+    MongoClient.connect(conn_str, function(err, database) {
+        if(err){
+            res.status(500).jsonp({error: 'We could not connect to the server.'});
+        }
+        db = database;
+        if (db && db !== "null" && db !== "undefined") {
     // list messages
-    db.collection('messages').find({}, { sort:[['_id', 'desc']]}, function(err, cursor) {
-      if (err) {
-        console.log(err.stack); 
-        
-        res.status(500).jsonp({error: 'mongodb message list failed.'});
-      } else {
-        cursor.toArray(function(err, items) {
-          if (err) {
-            console.log(err.stack); 
-            
-            res.status(500).jsonp({error: 'mongodb cursor to array failed.'});
-          } else {
-            
-            var diseases = new Array();
-            for(var item of items){
-                diseases.push({'message': item.message,'DISEASE':item.DISEASE});
+            db.collection('messages').find({}, { sort:[['_id', 'desc']]}, function(err, cursor) {
+            if (err) {
+                console.log(err.stack); 
+                
+                res.status(500).jsonp({error: 'mongodb message list failed.'});
+            } else {
+                cursor.toArray(function(err, items) {
+                if (err) {
+                    console.log(err.stack); 
+                    
+                    res.status(500).jsonp({error: 'mongodb cursor to array failed.'});
+                } else {
+                    
+                    var diseases = new Array();
+                    for(var item of items){
+                        diseases.push({'message': item.message,'DISEASE':item.DISEASE});
+                    }
+                    db.close();
+                    res.status(200).jsonp({data: diseases});
+                    
+                }
+                });
             }
-            res.status(200).jsonp({data: diseases});
+            });     
+        } else {
             
-          }
+            res.status(500).jsonp({error: 'No mongo found.'});
+        }
         });
-      }
-    });     
-  } else {
-    
-    res.status(500).jsonp({error: 'No mongo found.'});
-  }
+  
 });
 app.post('/api/insertMessage', function (req, res) {
-  var msg = req.body.message;
-  var arr;
-   relationship_extraction.extract({
-    text: msg,
-    dataset: 'ie-en-news' },
-    function (err, response) {
-        if (err){
-            console.log('error:', err);
-            arr = "Could not get the informations"
-            //res.status(500).jsonp({message: err});
-        }
-        else{
-           console.log(JSON.stringify(response.doc.entities.entity, null, 2));
-            arr = new Array();
-            for(var x of response.doc.entities.entity){
-                if(x.type == "DISEASE"){
-                    console.log("Type: "+x.type+" Text"+x.mentref[0].text);
-                    arr.push(x.mentref[0].text);    
-                }
-                
-            }
-        }
+    MongoClient.connect(conn_str, function(err, database) {
+        if(err){
+            res.status(500).jsonp({error: 'We could not connect to the server.'});
+        }  
+        db = database;
         
-        var message = { 'message': msg, 'DISEASE': arr,'ts': new Date() };
-        if (db && db !== "null" && db !== "undefined") {
-            db.collection('messages').insert(message, {safe:true}, function(err){
-            if (err) { 
-                console.log(err.stack);
-                res.write('mongodb message insert failed');
-                res.end(); 
+        
+        var msg = req.body.message;
+        var arr;
+        relationship_extraction.extract({
+        text: msg,
+        dataset: 'ie-en-news' },
+        function (err, response) {
+            if (err){
+                console.log('error:', err);
+                arr = ["Could not get the informations"];
+                //res.status(500).jsonp({message: "error"+err});
+            }
+            else{
+                console.log(JSON.stringify(response.doc.entities.entity, null, 2));
+                arr = new Array();
+                for(var x of response.doc.entities.entity){
+                    if(x.type == "DISEASE"){
+                        console.log("Type: "+x.type+" Text"+x.mentref[0].text);
+                        arr.push(x.mentref[0].text);    
+                    }
+                    
+                }
+            }
+            
+            var message = { 'message': msg, 'DISEASE': arr,'ts': new Date() };
+            if (db && db !== "null" && db !== "undefined") {
+                db.collection('messages').insert(message, {safe:true}, function(err){
+                if (err) { 
+                    console.log(err.stack);
+                    res.write('mongodb message insert failed');
+                    res.end(); 
+                } else {
+                    res.write('following messages has been inserted into database' + "\n" 
+                    + JSON.stringify(msg));
+                    res.end();
+                }
+                });    
             } else {
-                res.write('following messages has been inserted into database' + "\n" 
-                + JSON.stringify(msg));
+                res.write('No mongo found');
                 res.end();
             }
-            });    
-        } else {
-            res.write('No mongo found');
-            res.end();
-        }
-            
-            
         
-    });
+                
+        
+        });
+    
   
     
+  
+    });
    
 });
 
